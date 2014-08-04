e16a233e1fb76d396af6fa40055d2386256eb5c2
munimaev <munimaev@yandex.ru>
2014-08-04 10:19:35 +0400

    Update serv.js

--- serv.js
+++ serv.js
@@ -1,13 +1,26 @@
 var app = require('express')();
+var express  = require('express');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
+var SG = require('./servGame');
+var window = null;
 app.get('/', function(req, res){
-  res.sendfile('index.html');
+  res.sendfile(__dirname + '/index.html');
+});
+app.get('/game', function(req, res){
+  res.sendfile(__dirname + '/game.html');
+});
+app.get('/serv', function(req, res){
+  res.sendfile(__dirname + '/serv.html');
 });
 
+// app.use(express.cookieDecoder());
+// app.use(express.session());
+app.use('/public', express.static(__dirname + '/public'));
+
+
 io.on('connection', function(socket){
-  console.log('a user connected');
+  SG.initLobbi(io, socket);
 });
 
 http.listen(3000, function(){
