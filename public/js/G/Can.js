0aa5c61beb5ef8013ced6c6741c5a6fddb314848
munimaev <munimaev@yandex.ru>
2014-08-04 09:52:52 +0400

    Create Can.js

---
+++ public/js/G/Can.js
@@ -0,0 +1,212 @@
+
+function arraySearch(array, value) {
+    for ( var i in array) {
+        //console.log('msg',i,array[i], value)
+        if (array[i] == value) {
+            return i;
+        }
+    }
+    return null
+}
+var Can = {
+	pressNextBtn : function(o) {
+        if ( o.Stadies[o.S.phase].party == 'both') { 
+    	    if (o.pX == o.S.activePlayer) {
+    	    	if (o.meta.toNextPhase[o.pX] == false) {
+    	    		return true;
+    	    	} else {
+    	    		return false;
+    	    	}
+    	    } else {
+    	    	if (o.meta.toNextPhase[o.S.activePlayer] == true) {
+    	    		return true;
+    	    	} else {
+    	    		return false;
+    	    	}
+    	    }
+        }
+        if ( o.Stadies[o.S.phase].party == 'attacker') { 
+            if (o.pX == o.S.activePlayer) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        if ( o.Stadies[o.S.phase].party == 'blocker') { 
+            if (o.pX == o.S.activePlayer) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+
+	},
+	putInPlay : function(o) {
+        if ( o.S.phase == 'mission' 
+        	 && arraySearch(o.S[o.pX].hand, o.card) !== null
+        	 && o.Known[o.Accordance[o.card]].type == 'N' 
+        	 && o.Known[o.Accordance[o.card]].owner == o.pX 
+        	 && o.S.counters.playedNinjaActivePlayer == 0 ) {
+            return true;
+        }
+    },
+	charge : function(o) {
+        if ( o.S.phase == 'mission' 
+        	 && arraySearch(o.S[o.pX].hand, o.card) !== null
+        	 && o.Known[o.Accordance[o.card]].owner == o.pX) 
+        {
+            return true;
+        } 
+        else 
+        {
+        	console.log( o.S.phase == 'mission' 
+        	, arraySearch(o.S[o.pX].hand, o.card) !== null
+        	, o.Known[o.Accordance[o.card]].owner == o.pX
+            )
+			return false;
+        }
+    },
+    removeFromTeam : function(o){
+        if ( o.S.phase == 'organisation'
+             && o.Known[o.Accordance[o.card]].type == 'N' 
+             && o.Known[o.Accordance[o.card]].owner == o.pX 
+        ){
+            for (var i in o.S[o.pX].village.team) {
+                if (arraySearch(o.S[o.pX].village.team[i], o.card) !== null) {
+                    if (o.S[o.pX].village.team[i].length < 2) return false;  
+                    return true;
+                }
+            }
+        }
+        return false;
+    },
+    /**
+     * Можно ли поменять местами с этой картой
+     * @param  {[type]} o [description]
+     * @param  {[type]} o.S
+     * @param  {[type]} o.Known
+     * @param  {[type]} o.Accordance
+     * @param  {[type]} o.c1.card - первая выбранная
+     * @param  {[type]} o.c2.card - вторая выбранная
+     * @param  {[type]} o.pX
+     */
+    orgAddToTeam : function(o){
+        console.log(  o.S.phase == 'organisation'
+             , o.Known[o.Accordance[o.c1.card]].type == 'N' 
+             , o.Known[o.Accordance[o.c2.card]].type == 'N' 
+             , o.Known[o.Accordance[o.c1.card]].owner == o.pX 
+             , o.Known[o.Accordance[o.c2.card]].owner == o.pX )
+        if ( o.S.phase == 'organisation'
+             && o.Known[o.Accordance[o.c1.card]].type == 'N' 
+             && o.Known[o.Accordance[o.c2.card]].type == 'N' 
+             && o.Known[o.Accordance[o.c1.card]].owner == o.pX 
+             && o.Known[o.Accordance[o.c2.card]].owner == o.pX 
+        ){
+            for (var i in o.S[o.pX].village.team) {
+                if (arraySearch(o.S[o.pX].village.team[i], o.c2.card) !== null) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    },
+    /**
+     * Можно ли поменять местами с этой картой
+     * @param  {[type]} o [description]
+     * @param  {[type]} o.S
+     * @param  {[type]} o.Known
+     * @param  {[type]} o.Accordance
+     * @param  {[type]} o.card
+     * @param  {[type]} o.pX
+     */
+    orgChangeInTeam : function(o){
+        if ( o.S.phase == 'organisation'
+             && o.Known[o.Accordance[o.c1.card]].type == 'N' 
+             && o.Known[o.Accordance[o.c2.card]].type == 'N' 
+             && o.Known[o.Accordance[o.c1.card]].owner == o.pX 
+             && o.Known[o.Accordance[o.c2.card]].owner == o.pX 
+        ){
+            return true;
+        }
+        console.log(o.S.phase == 'organisation'
+             , o.Known[o.Accordance[o.c1.card]].type == 'N' 
+             , o.Known[o.Accordance[o.c2.card]].type == 'N' 
+             , o.Known[o.Accordance[o.c1.card]].owner == o.pX 
+             , o.Known[o.Accordance[o.c2.card]].owner == o.pX)
+        return false;
+    },
+    /**
+     * Можно ли поменять местами с этой картой
+     * @param  {[type]} o [description]
+     * @param  {[type]} o.S
+     * @param  {[type]} o.Known
+     * @param  {[type]} o.Accordance
+     * @param  {[type]} o.c1
+     * @param  {[type]} o.c1.card
+     * @param  {[type]} o.c2
+     * @param  {[type]} o.c2.card
+     * @param  {[type]} o.pX
+     */
+    organisation : function(o) {
+        if ( o.S.phase == 'organisation'
+             && o.Known[o.Accordance[o.c1.card]].type == 'N' 
+             && o.Known[o.Accordance[o.c2.card]].type == 'N' 
+             && o.Known[o.Accordance[o.c1.card]].owner == o.pX 
+             && o.Known[o.Accordance[o.c2.card]].owner == o.pX 
+        ){
+            return true;
+        }
+        console.log(o.S.phase == 'organisation'
+             , o.Known[o.Accordance[o.c1.card]].type == 'N' 
+             , o.Known[o.Accordance[o.c2.card]].type == 'N' 
+             , o.Known[o.Accordance[o.c1.card]].owner == o.pX 
+             , o.Known[o.Accordance[o.c2.card]].owner == o.pX)
+        return false;
+    },
+    /**
+     * @param  {[type]} o [description]
+     * @param  {[type]} o.S
+     * @param  {[type]} o.Known
+     * @param  {[type]} o.Accordance
+     * @param  {[type]} o.card
+     * @param  {[type]} o.pX
+     * @param  {[type]} o.team
+     * @param  {[type]} o.from
+     */
+    moveTeamToAttack : function (o) {
+        if ( o.S.phase == 'attack'
+             && o.Known[o.Accordance[o.card]].type  == 'N' 
+             && o.Known[o.Accordance[o.card]].owner == o.pX 
+             && o.S.activePlayer == o.pX
+             && o.S[o.pX][o.from].team[o.team]
+             && arraySearch(o.S[o.pX][o.from].team[o.team], o.card) !== null
+        ){
+            return true;
+        }
+        return false;
+    },
+    moveTeamToBlock : function (o) {
+        if ( o.S.phase == 'block'
+             && o.Known[o.Accordance[o.card]].type  == 'N' 
+             && o.Known[o.Accordance[o.card]].owner == o.pX 
+             && o.S.activePlayer != o.pX
+             && o.S[o.pX][o.from].team[o.team]
+             && arraySearch(o.S[o.pX][o.from].team[o.team], o.card) !== null
+        ){
+            return true;
+        }
+        return false;
+    },
+    block : function(o) {
+        if (o.pX != o.S.activePlayer
+            && o.attackTeam in o.S[o.S.activePlayer].attack.team
+            && (o.blockTeam in o.S[o.pX].block.team 
+                || o.blockTeam in o.S[o.pX].village.team)
+            ) {
+            return true;
+        }
+    }
+}
+if (module) {
+	module.exports = Can;
+}
