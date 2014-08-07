8361948765d4304d875af8cfbfb725302868569d
munimaev <munimaev@yandex.ru>
2014-08-07 17:45:41 +0400

    Update ready.js

--- public/js/G/ready.js
+++ public/js/G/ready.js
@@ -1,7 +1,7 @@
 var module = null;
 var H = {
-    you : {deck:{},discard:{},chackra:{}},
-    opp : {deck:{},discard:{},chackra:{}},
+    you : {deck:{},discard:{},chackra:{},rewards:{}},
+    opp : {deck:{},discard:{},chackra:{},rewards:{}},
     phase : null,
 };
 var ready = false;
@@ -18,9 +18,13 @@ $( document ).ready(function() {
     H.you.deck.span    = $("#youBar .deck span");
     H.you.discard.span = $("#youBar .discard span");
     H.you.chackra.span = $("#youBar .chackra span");
+    H.you.rewards.span = $("#youBar .rewards span");
+    H.you.rewards.pic  = $("#youBar .rewards pic");
     H.opp.deck.span    = $("#oppBar .deck span");
     H.opp.discard.span = $("#oppBar .discard span");
     H.opp.chackra.span = $("#oppBar .chackra span");
+    H.opp.rewards.span = $("#oppBar .rewards span");
+    H.opp.rewards.pic  = $("#oppBar .rewards pic");
     H.next = $("#next");
     H.phase = $("#phase");
     H.animate = $("#animate");
