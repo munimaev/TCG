0259999c8cfce94b7c9d11d032241880533c77b8
munimaev <munimaev@yandex.ru>
2014-08-07 17:45:17 +0400

    Update socket.js

--- public/js/G/socket.js
+++ public/js/G/socket.js
@@ -35,12 +35,17 @@ function updAreaCount(player, area) {
 	var count =  S[Client[player]][area].length;
 	H[player][area].span.html(count)
 }
+function updRewardsCount(player) {
+	var count =  S[Client[player]].rewards;
+	H[player].rewards.span.html(count)
+}
 function updAllCount() {
 	var P, A;
 	for (var p in P = ['you','opp']) {
 		for (var a in A = ['deck', 'discard', 'chackra']) {
 			updAreaCount(P[p],A[a]);
 		}
+		updRewardsCount(P[p]);
 	}
 }
 
@@ -70,7 +75,7 @@ function get_arg(txt) {
 
 function applyUpd(d) {
 	if (d.upd) {
-		console.log("↳ upd ",d.upd)
+//		console.log("↳ upd ",d.upd)
 		if (d.upd.known) for (var i in d.upd.known) {
 			Known[i] = d.upd.known[i];
 		}
@@ -92,7 +97,7 @@ socket.on('action',function(d) {
 
 function applyAct(d) {
 	if (d.acts) {
-		console.log("↳ acts ",d.acts)
+//		console.log("↳ acts ",d.acts)
 		for (var i in d.acts) {
 			for (var i2 in d.acts[i].arg) {
 				d.acts[i].arg[i2] = get_arg(d.acts[i].arg[i2])
