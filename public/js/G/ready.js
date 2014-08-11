1ee3556554efe1536e9110055ac66592e0a0ab48
munimaev <munimaev@yandex.ru>
2014-08-11 17:52:22 +0400

    Update ready.js

--- public/js/G/ready.js
+++ public/js/G/ready.js
@@ -1,7 +1,7 @@
 var module = null;
 var H = {
-    you : {deck:{},discard:{},chackra:{},rewards:{}},
-    opp : {deck:{},discard:{},chackra:{},rewards:{}},
+    you : {deck:{},discard:{},chackra:{},rewards:{},turnCounter:{}},
+    opp : {deck:{},discard:{},chackra:{},rewards:{},turnCounter:{}},
     phase : null,
 };
 var ready = false;
@@ -20,11 +20,15 @@ $( document ).ready(function() {
     H.you.chackra.span = $("#youBar .chackra span");
     H.you.rewards.span = $("#youBar .rewards span");
     H.you.rewards.pic  = $("#youBar .rewards pic");
+    H.you.turnCounter.span = $("#youBar .turnCounter span");
+    H.you.turnCounter.pic  = $("#youBar .turnCounter pic");
     H.opp.deck.span    = $("#oppBar .deck span");
     H.opp.discard.span = $("#oppBar .discard span");
     H.opp.chackra.span = $("#oppBar .chackra span");
     H.opp.rewards.span = $("#oppBar .rewards span");
     H.opp.rewards.pic  = $("#oppBar .rewards pic");
+    H.opp.turnCounter.span = $("#oppBar .turnCounter span");
+    H.opp.turnCounter.pic  = $("#oppBar .turnCounter pic");
     H.next = $("#next");
     H.phase = $("#phase");
     H.animate = $("#animate");
