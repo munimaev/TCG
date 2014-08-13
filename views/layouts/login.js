fa3fee4d61f6ec7fd13853eb8a7b1d61947fbefd
munimaev <munimaev@yandex.ru>
2014-08-13 17:56:38 +0400

    Create login.js

---
+++ views/layouts/login.js
@@ -0,0 +1,12 @@
+<h1>Логин</h1>
+<form role="form">
+  <div class="form-group">
+    <label for="exampleInputEmail1">Login</label>
+    <input type="text" class="form-control" name="login" placeholder="Login">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" name="password" placeholder="Password">
+  </div>
+  <button type="submit" class="btn btn-default">Submit</button>
+</form>
