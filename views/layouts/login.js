bae308b6c14919b40a97404376151b312088b294
munimaev <munimaev@yandex.ru>
2014-12-30 17:53:35 +0300

    Update login.js

--- views/layouts/login.js
+++ views/layouts/login.js
@@ -2,11 +2,12 @@
 <form role="form">
   <div class="form-group">
     <label for="exampleInputEmail1">Login</label>
-    <input type="text" class="form-control" name="login" placeholder="Login">
+    <input type="text" class="form-control" name="login" placeholder="Login" value="Thor">
   </div>
   <div class="form-group">
     <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" name="password" placeholder="Password">
+    <input type="password" class="form-control" name="password" placeholder="Password" value="111">
   </div>
   <button type="submit" class="btn btn-default">Submit</button>
+  <button type="submit" class="btn btn-link"><a href="/login?login=Odin&password=111">Odin</a></button>
 </form>
