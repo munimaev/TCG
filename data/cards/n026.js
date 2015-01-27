45ab3704fba59b100312fc367b2d03a76d084423
munimaev <munimaev@yandex.ru>
2015-01-27 15:31:53 +0300

    Create n026.js

---
+++ data/cards/n026.js
@@ -0,0 +1,29 @@
+var n026 = {
+  "type": "N",
+  "ec": 1,
+  "hc": 0,
+  "ah": 3,
+  "sh": 0,
+  "ai": 1,
+  "si": 0,
+  "img": "n026",
+  "number": "n026",
+  "elements": "F",
+  "name": "Саске Учиха",
+  "effectText": {
+    "effectName": "Открой галаза!",
+    "effects": [{
+      "effect": "Этот ниндзя не может быть целью для техник оппонента со стоимостью 2 или меньше (символ стихии считается как 1)."
+    }]
+  },
+  "statuses": [],
+  "atributes": ["Fire"],
+  "effect": {
+    "static": {
+      "selfPower": [{}]
+    }
+  }
+}
+if (module) {
+  exports.card = n026;
+}
