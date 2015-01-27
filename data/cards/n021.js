61a518a02c2e0165c82cab023f25a6a3886bcb99
munimaev <munimaev@yandex.ru>
2015-01-27 15:31:18 +0300

    Create n021.js

---
+++ data/cards/n021.js
@@ -0,0 +1,29 @@
+var n021 = {
+  "type": "N",
+  "ec": 1,
+  "hc": 0,
+  "ah": 1,
+  "sh": 1,
+  "ai": 0,
+  "si": 1,
+  "img": "n021",
+  "number": "n021",
+  "elements": "W",
+  "name": "Сакура Харуно",
+  "effectText": {
+    "effectName": "Техника любовных соперниц",
+    "effects": [{
+      "effect": "Когда этот ниндзя сражается в одной команде с ниндзя имеющим статус Генин и Мужской, этот ниндзя солучает +0/+1 до конца хода."
+    }]
+  },
+  "statuses": [],
+  "atributes": ["Weapon"],
+  "effect": {
+    "static": {
+      "selfPower": [{}]
+    }
+  }
+}
+if (module) {
+  exports.card = n021;
+}
