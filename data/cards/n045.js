1e19fd1e718030d65bbd0b50c745d1ef858ccea3
munimaev <munimaev@yandex.ru>
2015-01-27 15:33:13 +0300

    Create n045.js

---
+++ data/cards/n045.js
@@ -0,0 +1,29 @@
+var n045 = {
+  "type": "N",
+  "ec": 1,
+  "hc": 0,
+  "ah": 1,
+  "sh": 1,
+  "ai": 0,
+  "si": 0,
+  "img": "n045",
+  "number": "n045",
+  "elements": "A",
+  "name": "Кабуто Якуши",
+  "effectText": {
+    "effectName": "Собрание информации",
+    "effects": [{
+      "effect": "Когда этот ниндзя входит в игру, посмотрите верхнюю карту вашей колоды и верните ее наверх или вниз колоды."
+    }]
+  },
+  "statuses": [],
+  "atributes": ["Ninjutsu"],
+  "effect": {
+    "static": {
+      "selfPower": [{}]
+    }
+  }
+}
+if (module) {
+  exports.card = n045;
+}
