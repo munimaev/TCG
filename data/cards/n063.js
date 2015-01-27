ea840f387b76e49186bc3ed64a971d1e3d222422
munimaev <munimaev@yandex.ru>
2015-01-27 15:33:40 +0300

    Create n063.js

---
+++ data/cards/n063.js
@@ -0,0 +1,29 @@
+var n063 = {
+  "type": "N",
+  "ec": 2,
+  "hc": 0,
+  "ah": 3,
+  "sh": 1,
+  "ai": 2,
+  "si": 1,
+  "img": "n063",
+  "number": "n063",
+  "elements": "L",
+  "name": "Ирука Умино",
+  "effectText": {
+    "effectName": "Приверженец оружия",
+    "effects": [{
+      "effect" : "Когда этот ниндзя входит в игру, исцелите 1 вашего раненного ниндзя."
+    }]
+  },
+  "statuses": [],
+  "atributes": ["Weapon"],
+  "effect": {
+    "static": {
+      "selfPower": [{}]
+    }
+  }
+}
+if (module) {
+  exports.card = n063;
+}
