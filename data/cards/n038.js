f8b280e90080f8ade8aa7a52ac64f28931f634ec
munimaev <munimaev@yandex.ru>
2015-01-27 15:32:17 +0300

    Create n038.js

---
+++ data/cards/n038.js
@@ -0,0 +1,27 @@
+var n038 = {
+  "type": "N",
+  "ec": 4,
+  "hc": 0,
+  "ah": 4,
+  "sh": 3,
+  "ai": 2,
+  "si": 3,
+  "img": "n038",
+  "number": "n038",
+  "elements": "W",
+  "name": "Куренай Юхи",
+  "effectText": {
+    "effectName": null,
+    "effects": []
+  },
+  "statuses": [],
+  "atributes": ["Genjutsu"],
+  "effect": {
+    "static": {
+      "selfPower": [{}]
+    }
+  }
+}
+if (module) {
+  exports.card = n038;
+}
