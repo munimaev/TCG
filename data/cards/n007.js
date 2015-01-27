417b1f49ad980aefd616b190df147836819fdaf4
munimaev <munimaev@yandex.ru>
2015-01-27 15:31:00 +0300

    Create n007.js

---
+++ data/cards/n007.js
@@ -0,0 +1,30 @@
+var n007 = {
+  "type": "N",
+  "ec": 0,
+  "hc": 0,
+  "ah": 0,
+  "sh": 0,
+  "ai": 1,
+  "si": 0,
+  "img": "n007",
+  "number": "n007",
+  "elements": "L",
+  "name": "Конохамару",
+  "effectText": {
+    "effectName": "Внук хокаге",
+    "effects": [{
+      "valid" : true,
+      "effect": "Все ниндзя в вашей руке с рангом Джонин или выше получают -1 к цене выхода."
+    }]
+  },
+  "statuses": [],
+  "atributes": ["Ninjutsu"],
+  "effect": {
+    "static": {
+      "selfPower": [{}]
+    }
+  }
+}
+if (module) {
+  exports.card = n007;
+}
