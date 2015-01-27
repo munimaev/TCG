33d7d6b3fd466c222fc559d2212c2119fb322e41
munimaev <munimaev@yandex.ru>
2015-01-27 15:30:43 +0300

    Create n005.js

---
+++ data/cards/n005.js
@@ -0,0 +1,30 @@
+var n005 = {
+  "type": "N",
+  "ec": 0,
+  "hc": 0,
+  "ah": 0,
+  "sh": 2,
+  "ai": 0,
+  "si": 1,
+  "img": "n005",
+  "number": "n005",
+  "elements": "E",
+  "name": "Шикамару Нара",
+  "effectText": {
+    "effectName": "Лентяй",
+    "effects": [{
+      "valid" : true,
+      "effect": "Этот ниндзя не может быть отпралвен в атаку один."
+    }]
+  },
+  "statuses": [],
+  "atributes": ["Shadow"],
+  "effect": {
+    "static": {
+      "selfPower": [{}]
+    }
+  }
+}
+if (module) {
+  exports.card = n005;
+}
